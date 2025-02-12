820c40954f3aa9dc5a8923b8167d22ebfea82b1e
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-12 03:54:41 -0500

    ✨ Refactor Swagger configuration to use external swaggerConfig

--- src/config/swagger.js
+++ src/config/swagger.js
@@ -1,35 +1,4 @@
 import swaggerJsdoc from 'swagger-jsdoc';
+import { swaggerConfig } from '../docs/index.js';
 
-const options = {
-	definition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'Discord Messages API',
-			version: '1.0.0',
-			description: 'API for scheduling and managing Discord messages',
-		},
-		servers: [
-			{
-				url: 'http://localhost:4000',
-				description: 'Development server',
-			},
-		],
-		components: {
-			securitySchemes: {
-				BearerAuth: {
-					type: 'http',
-					scheme: 'bearer',
-					bearerFormat: 'JWT',
-				},
-			},
-		},
-		security: [
-			{
-				BearerAuth: [],
-			},
-		],
-	},
-	apis: ['./src/routes/*.js'],
-};
-
-export const specs = swaggerJsdoc(options);
+export const specs = swaggerJsdoc(swaggerConfig);
