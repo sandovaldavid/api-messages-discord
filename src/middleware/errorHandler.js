a0f69f03fc117a381e702ff2f6b84e660de19946
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-12 02:26:39 -0500

    ✨ Remove custom APIError class from error handler middleware

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -65,13 +65,3 @@ export const errorHandler = (err, req, res, next) => {
 
 	res.status(err.statusCode).json(errorResponse);
 };
-
-// Customized error class
-export class APIError extends Error {
-	constructor(message, statusCode = 500) {
-		super(message);
-		this.statusCode = statusCode;
-		this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
-		Error.captureStackTrace(this, this.constructor);
-	}
-}
