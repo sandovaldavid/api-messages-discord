e9afe3f378bb3c74d2e2352c210937e3107ea920
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-12 00:59:49 -0500

    ✨ Add centralized error handling and improve request logging middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import logger from './utils/logger.js';
+import { errorHandler, APIError } from './middleware/errorHandler.js';
 
 dotenv.config();
 
@@ -9,7 +10,15 @@ const port = process.env.PORT || 3000;
 
 // Middleware para loggear peticiones HTTP
 app.use((req, res, next) => {
-	logger.info(`${req.method} ${req.url}`);
+	const start = Date.now();
+
+	app.set('trust proxy', true);
+
+	res.on('finish', () => {
+		const responseTime = Date.now() - start;
+		logger.http(req, res, responseTime);
+	});
+
 	next();
 });
 
@@ -18,6 +27,13 @@ app.use(express.json());
 app.get('/', (req, res) => {
 	res.json({ message: 'Discord Messages API' });
 });
+
+app.all('*', (req, res, next) => {
+	next(new APIError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Middleware de manejo de errores
+app.use(errorHandler);
 
 app.listen(port, () => {
 	logger.info(`Server running on port ${port}`);
