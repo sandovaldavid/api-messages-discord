0b664c2484b28c2fc51a3e3749d4a82c1180d191
sandovaldavid <sandovaldavid2201@gmail.com>
2025-02-13 16:36:05 -0500

    ♻️ enhance getFormattedScheduledFor method to support timezone parameter

--- src/models/Message.js
+++ src/models/Message.js
@@ -47,14 +47,19 @@ messageSchema.methods.reschedule = async function (newTime) {
 	return this;
 };
 
-messageSchema.methods.getFormattedScheduledFor = function (locale = 'en-US') {
+messageSchema.methods.getFormattedScheduledFor = function (
+	locale = 'en-US',
+	timezone = 'UTC'
+) {
 	return this.scheduledFor.toLocaleString(locale, {
+		timeZone: timezone,
 		weekday: 'long',
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric',
 		hour: '2-digit',
 		minute: '2-digit',
+		timeZoneName: 'short',
 	});
 };
 
